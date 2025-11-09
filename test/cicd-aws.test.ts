4d4d0093619af83a50c0f49c67322ff7cc3718a9
krishan nandan sharma <krishannandan09@gmail.com>
2025-11-09 19:42:26 +0530

    checking bad code

--- test/cicd-aws.test.ts
+++ test/cicd-aws.test.ts
@@ -4,7 +4,7 @@ describe("Hello describes test suite", () => {
 
     test('handler should return 200', async () => {
         const result = await handler({}, {});
-        expect((result.statusCode)).toBe(200);
+        expect((result.statusCode)).toBe(400);
 
     })
 });
